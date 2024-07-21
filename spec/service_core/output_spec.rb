58dcc4bdaf7d63e15cdd61bc1c56f138c53a4b61
mayank sehgal <sehgalmayank001@gmail.com>
2024-07-21 10:03:11 +0530

    debump & fix rubocop

--- spec/service_core/output_spec.rb
+++ spec/service_core/output_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "spec_helper"
 
 class OutputTestService
@@ -12,55 +14,57 @@ class OutputTestService
   end
 end
 
-
 RSpec.describe ServiceCore::Output do
   let(:service) { OutputTestService.new }
 
-  describe '#initialize' do
-    it 'initializes the output with default status' do
+  describe "#initialize" do
+    it "initializes the output with default status" do
       expect(service.output).to eq({ status: "initialized" })
     end
   end
 
-  describe '#set_output' do
-    context 'when key is valid' do
-      it 'sets the output value' do
+  describe "#set_output" do
+    context "when key is valid" do
+      it "sets the output value" do
         service.set_test_output(:data, "payload")
         expect(service.output[:data]).to eq("payload")
         expect(service.output[:status]).to eq("initialized")
       end
 
-      it 'sets the status_dirty to true if key is :status' do
+      it "sets the status_dirty to true if key is :status" do
         service.set_test_output(:status, "error")
         expect(service.output[:status]).to eq("error")
       end
     end
 
-    context 'when key is invalid' do
-      it 'raises an ArgumentError' do
-        expect { service.set_test_output(:invalid_key, "value") }.to raise_error(ArgumentError, "Invalid key. Allowed keys are: status, data, message, errors")
+    context "when key is invalid" do
+      it "raises an ArgumentError" do
+        expect do
+          service.set_test_output(:invalid_key,
+                                  "value")
+        end.to raise_error(ArgumentError, "Invalid key. Allowed keys are: status, data, message, errors")
       end
     end
   end
 
-  describe '#auto_assign_status' do
-    context 'when output is not dirty' do
-      it 'sets status to success' do
+  describe "#auto_assign_status" do
+    context "when output is not dirty" do
+      it "sets status to success" do
         service.test_auto_assign_status
         expect(service.output[:status]).to eq("success")
       end
     end
 
-    context 'when output is dirty and errors are blank' do
-      it 'sets status to success' do
+    context "when output is dirty and errors are blank" do
+      it "sets status to success" do
         service.set_test_output(:data, "payload")
         service.test_auto_assign_status
         expect(service.output[:status]).to eq("success")
       end
     end
 
-    context 'when output is dirty and errors are present' do
-      it 'sets status to error' do
+    context "when output is dirty and errors are present" do
+      it "sets status to error" do
         service.set_test_output(:errors, "something went wrong")
         service.test_auto_assign_status
         expect(service.output[:status]).to eq("error")
